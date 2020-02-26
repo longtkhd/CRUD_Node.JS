b9d7047efafe1c2ea4d7950736630adaa55871f5
longtkhd <longtkhd2709@gmail.com>
2020-02-26 11:29:03 +0700

    delete user by id

--- app.js
+++ app.js
@@ -75,13 +75,11 @@ app.get('/user/:id', async (req,res) => {
     
     const user = await User.findById(id);
     if(!user){
-      return res.status(404).send();
+      return  res.status(404).send();
     }
-
-    res.send(user);
+              res.send(user);
   }catch(e){
-    res.status(500).send(e);
-
+              res.status(500).send(e);
   }
 
 
@@ -104,10 +102,22 @@ app.patch('/user/:id', async (req,res) => {
     }
     res.send(user);
     
-
   }catch(e){
     res.send(e);
 
+  }
+})
+
+//delete
+app.post('/user/:id', async (req,res) => {
+  try{
+    const user = await User.findByIdAndDelete(req.params.id);
+    if(!user){
+      res.status(404).send();
+    }
+      res.send(user);
+  }catch(e){
+      res.status(500).send(e);
   }
 })
 
