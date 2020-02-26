3337de876586c9a8b1616e79bf38d4b867689eac
longtkhd <longtkhd2709@gmail.com>
2020-02-26 10:21:52 +0700

    getUserById API

--- app.js
+++ app.js
@@ -54,11 +54,28 @@ app.post('/user', async (req,res)  => {
 app.get('/user', async (req,res) => {
   try{
     const users = await User.find({});
-    res.status(201).send(users);
+    res.status(200).send(users);
   }catch(e){
     res.status(500).send(e);
 
   }
+})
+app.get('/user/:id', async (req,res) => {
+  const id = req.params.id;
+  try{
+    
+    const user = await User.findById(id);
+    if(!user){
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  }catch(e){
+    res.status(500).send(e);
+
+  }
+
+
 })
 
 
